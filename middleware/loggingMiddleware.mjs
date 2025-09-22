c62e3fb175ba3bc69042f2da69cae0d97e8818c1
Elsa S <elsasalinas2388@gmail.com>
2025-09-22 10:47:35 -0400

    worksonpostman

--- middleware/loggingMiddleware.mjs
+++ middleware/loggingMiddleware.mjs
@@ -1,3 +1,5 @@
-export default (req, res, next) => {
-    console.log(`${req.method} - ${req.path}`)
+const logger = (req, res, next) => {
+    console.log(`${req.method} - ${req.path}`);
+    next();
 };
+export default logger; 
