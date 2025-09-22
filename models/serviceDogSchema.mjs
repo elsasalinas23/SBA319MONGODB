35dcc1e6aa2f20bd3ccb475d7a00577ae7f8bfe5
Elsa S <elsasalinas2388@gmail.com>
2025-09-22 14:56:18 -0400

    lasttouch

--- models/serviceDogSchema.mjs
+++ models/serviceDogSchema.mjs
@@ -4,9 +4,10 @@ const ServiceDogschema = new mongoose.Schema({
     name:{type: String, required: true},
     species: {type: String, required: true},
     age: {type: Number, required: true},
-    habitat: {type: String},
-
+    habitat: {type: String,
+        enum: { values: ["apartment", "house","farmhouse","sharedroom"],required : true,},
 });
+
 
 
 ServiceDogschema.index({species: 1});
